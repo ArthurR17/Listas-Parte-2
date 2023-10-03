a97eb374752549fde5787265cbe6beba9d6720ab
Arthur Rodrigues Santos <arthur48.santos@hotmail.com>
2023-10-03 09:56:06 -0300

    Update exercicio05.py

--- exercicio05.py
+++ exercicio05.py
@@ -1,22 +1,25 @@
 ultimo = 10
 fila = list(range(1, ultimo + 1))
 
-for e in fila:
-    print(f"\nExistem %d clientes na fila" % len(fila))
+while True:
+    if not fila:
+        print("Fila vazia. Encerrando o atendimento.")
+        break
+
+    print(f"\nExistem {len(fila)} clientes na fila")
     print("Fila atual: ", fila)
     print("Digite F para adicionar um cliente ao fim da fila,")
     print("ou A para realizar o atendimento. S para sair.")
     operacao = input("Operação (F, A ou S): ")
+
     if operacao == "A":
-        if len(fila) > 0:
-            atendido = fila.pop(0)
-            print("Cliente %d atendido" % atendido)
-        else:
-            print("Fila vazia! Ninguém para atender.")
+        atendido = fila.pop(0)
+        print(f"Cliente {atendido} atendido")
     elif operacao == "F":
         ultimo += 1
         fila.append(ultimo)
     elif operacao == "S":
+        print("Encerrando o atendimento.")
         break
     else:
         print("Operação inválida! Digite apenas F, A ou S!")
